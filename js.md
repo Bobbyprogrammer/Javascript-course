6ac4cbbf9c52e02c9cbf1103ee0878a23db3defa
Bobbyprogrammer <babardev167@gmail.com>
2026-07-08 23:29:53 +0500

    first commit

---
+++ js.md
@@ -0,0 +1,22 @@
+# **What is a Variable?**
+
+# **Data Types**
+
+<!-- Assignment -->
+
+1. Print "Hello JavaScript" using console.log().
+2. Create variables for:
+   name
+   age
+   city
+3. Print all the variables.
+4. Create one variable of each type:
+   String
+   Number
+   Boolean
+   Undefined
+   Null
+   Object
+   Array
+
+5. Use typeof to print the type of each variable.
---
+++ variable.js
@@ -0,0 +1,49 @@
+// let name = "Babar";
+// let number = 3;
+// console.log(typeof number);
+
+// let isLoggedIn = false;
+// if (isLoggedIn === true) {
+//   console.log("You are loggedin");
+// } else {
+//   console.log("You are not loggedin");
+// }
+
+// console.log(typeof isLoggedIn);
+
+// undefined
+
+// let name;
+
+// console.log(name);
+// Null
+
+// let user = null;
+
+// console.log(typeof user)
+
+// object
+
+// let student = {
+//   name: "Babar",
+//   age: 25,
+//   gender: "Male",
+// };
+
+// student.name = "Babar ALi";
+
+// console.log(student.name);
+
+// Array
+
+// let colors = ["Red", "Green", "Yellow", 23, true, false];
+
+// console.log(colors[0]);
+// console.log(colors[1]);
+// console.log(colors[2]);
+
+console.log(typeof "Ali");
+console.log(typeof 10);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
